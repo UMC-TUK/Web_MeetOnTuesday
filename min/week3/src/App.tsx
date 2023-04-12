3054890b96d846c4ab9e5ee355e25bfe3e0942c0
minjilee <budlalswl1129@gmail.com>
2023-04-12 16:02:17 +0900

    chore: 리액트 초기세팅(ESLint, Prettier, TS적용)

---
+++ min/week3/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PlayGroundPage from 'pages/PlaygroundPage';
+
+function App() {
+  return (
+    <div>
+      <PlayGroundPage />
+    </div>
+  );
+}
+
+export default App;
