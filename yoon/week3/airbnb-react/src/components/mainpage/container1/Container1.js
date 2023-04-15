33503a8c377d826a20ea859a6c8e65e6da36c422
parkyoon <parkyoon97@gmail.com>
2023-04-16 00:14:34 +0900

    react

---
+++ yoon/week3/airbnb-react/src/components/mainpage/container1/Container1.js
@@ -0,0 +1,7 @@
+import classes from './Container1.module.css';
+
+const Container1 = () => {
+  return (<div className={classes.container1}>Container1</div>);
+};
+
+export default Container1;
---
+++ yoon/week3/airbnb-react/src/components/mainpage/container2/Container2.js
@@ -0,0 +1,7 @@
+import classes from './Container2.module.css';
+
+const Container2 = () => {
+  return (<div className={classes.container2}>Container2</div>);
+};
+
+export default Container2;
