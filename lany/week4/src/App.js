5510261f5caf313dc7a3b3d65ed7d820c2e1fc57
gs0428 <lider28@naver.com>
2023-04-20 02:20:02 +0900

    week4_homework

--- lany/week4/src/App.js
+++ lany/week4/src/App.js
@@ -4,6 +4,10 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Market from "./pages/Market";
 import Beauty from "./pages/Beauty";
 import Root from "./pages/Root";
+import Best from "./pages/Best";
+import NewProducts from "./pages/NewProducts";
+import TimeSales from "./pages/TimeSales";
+import Benefits from "./pages/Benefits";
 
 function App() {
   const [isMarket, setIsMarket] = useState(true);
@@ -20,6 +24,16 @@ function App() {
       path: "/beauty",
       element: <Root />,
       children: [{ path: "", element: <Beauty isMarket={isMarket} setLocation={setLocation} /> }],
+    },
+    {
+      path: isMarket ? "/" : "/beauty",
+      element: <Root />,
+      children: [
+        { path: "best", element: <Best isMarket={isMarket} /> },
+        { path: "newproducts", element: <NewProducts isMarket={isMarket} /> },
+        { path: "timesales", element: <TimeSales isMarket={isMarket} /> },
+        { path: "benefits", element: <Benefits isMarket={isMarket} /> },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
