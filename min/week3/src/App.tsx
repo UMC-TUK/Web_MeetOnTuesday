67f0ff8d64701c8a0132282b258080bb89774b70
minjilee <budlalswl1129@gmail.com>
2023-05-07 03:32:15 +0900

    chore: 폴더 분리

--- min/week3/src/App.tsx
+++ min/week3/src/App.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainPage from 'pages/MainPage';
-import OtherPage from 'pages/OtherPage';
-import Root from 'pages/Root';
-import ErrorPage from 'pages/ErrorPage';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: '/', element: <MainPage /> },
-      { path: '/other/:content', element: <OtherPage /> },
-    ],
-  },
-]);
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <div>
+      <MainPage />
+    </div>
   );
 }
 
