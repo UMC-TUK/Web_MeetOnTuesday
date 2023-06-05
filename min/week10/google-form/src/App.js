287970bc351ae0c753e0a7bd03ec65ef5ba1c9b8
minjilee <budlalswl1129@gmail.com>
2023-06-06 02:48:34 +0900

    feat: react-hook-form 이용한 구글폼 구현

---
+++ min/week10/google-form/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import FormHeader from "./components/FormHeader";
+import Header from "./components/GoogleFromHeader";
+import Form from "./components/FormContent";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <FormHeader />
+      <Form />
+    </>
+  );
+}
+
+export default App;
